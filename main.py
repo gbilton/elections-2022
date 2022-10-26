b1b704dab13ac8d73400aba6c47a842e068504df
= <=>
2022-10-26 20:51:44 -0300

    docstring

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ app.add_middleware(
 
 @app.get("/predictions", response_model=list[Prediction])
 async def get_predictions():
+    """Fetch all predictions
+
+    Returns:
+        list[Prediction]: A list of predictions
+    """
     prediction_service = PredictionService()
     predictions = prediction_service.get_all_predictions()
     return predictions
@@ -27,6 +32,11 @@ async def get_predictions():
 
 @app.get("/predictions/last", response_model=Prediction)
 async def get_last_prediction():
+    """Fetch the last prediction
+
+    Returns:
+        Prediction: The last prediction
+    """
     prediction_service = PredictionService()
     last_prediction = prediction_service.get_last_prediction()
     return last_prediction
